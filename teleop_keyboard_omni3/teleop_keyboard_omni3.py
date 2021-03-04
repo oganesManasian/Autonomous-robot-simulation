d2048e47398b101a7f53c7a75b29f2f50a11119b
Oganes Manasian <oganesman8@gmail.com>
2021-03-04 18:41:34 +0100

    Cleaned source files related to control

--- teleop_keyboard_omni3/teleop_keyboard_omni3.py
+++ teleop_keyboard_omni3/teleop_keyboard_omni3.py
@@ -8,7 +8,7 @@ from std_msgs.msg import Float64
 
 import sys, select, termios, tty
 
-msg = """
+help_info = """
 Reading from the keyboard !
 ---------------------------
 Moving around:
@@ -30,7 +30,7 @@ q/z : increase/decrease max speeds by 10%
 CTRL-C to quit
 """
 
-moveBindings = {
+MOVE_BINDINGS = {
         'i':(-1,0,1),
         'o':(-1,1,0),
         'j':(1,1,1),
@@ -39,8 +39,9 @@ moveBindings = {
         ',':(1,0,-1),
         '.':(0,1,-1),
         'm':(1,-1,0),  
-        'O':(-1,1,0),
+
         'I':(-1,0,1),
+        'O':(-1,1,0),
         'J':(1,-2,1),
         'L':(-1,2,-1),
         'U':(0,-1,1),
@@ -49,10 +50,12 @@ moveBindings = {
         'M':(1,-1,0),  
     }
 
-speedBindings={
-        'q':(1.1,1.1),
-        'z':(.9,.9),
+SPEED_BINDINGS={
+        'q':1.1,
+        'z':.9,
     }
+
+PUBLISH_RATE = 2
 
 def getKey():
     tty.setraw(sys.stdin.fileno())
@@ -61,8 +64,8 @@ def getKey():
     return key
 
 
-def vels(speed):
-    return "currently:\tspeed %s " % (speed)
+def print_speed(speed):
+    return "Currently:\tspeed %s " % (speed)
 
 if __name__=="__main__":
     settings = termios.tcgetattr(sys.stdin)
@@ -74,44 +77,34 @@ if __name__=="__main__":
 
 
     speed = 1.0
-    x = 0
-    y = 0
-    z = 0
-    status = 0
+    rate = rospy.Rate(PUBLISH_RATE)
 
     try:
-        print(msg)
-        print(vels(speed))
-        while(1):
+        while True:
+            print(help_info)
+            print(print_speed(speed))
+
             key = getKey()
-            print("Read key:", key)
-            if key in moveBindings.keys():
-                x = moveBindings[key][0]
-                y = moveBindings[key][1]
-                z = moveBindings[key][2]
-            elif key in speedBindings.keys():
-                speed = speed * speedBindings[key][0]
-       
-                print(vels(speed))
-                if (status == 14):
-                    print(msg)
-                status = (status + 1) % 15
-            else:
+            if key in MOVE_BINDINGS.keys():
+                x = MOVE_BINDINGS[key][0]
+                y = MOVE_BINDINGS[key][1]
+                z = MOVE_BINDINGS[key][2]
+            elif key in SPEED_BINDINGS.keys():
+                speed = speed * SPEED_BINDINGS[key]
+            else: # Any other key => stop
                 x = 0
                 y = 0
                 z = 0
-                th = 0
                 if (key == '\x03'):
                     break
             
             vell = Float64()
-        
             velb = Float64()
             velr = Float64()
             
-            vell = x*speed
-            velb = y*speed
-            velr = z*speed
+            vell = x * speed
+            velb = y * speed
+            velr = z * speed
             
             publ.publish(vell)
             pubb.publish(velb)
