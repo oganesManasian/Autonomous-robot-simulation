ff1ab6dde3f93276de21a5bfe58c8a05458f54fa
Oganes Manasian <oganesman8@gmail.com>
2021-04-22 18:26:30 +0200

    Changed platform model

--- teleop_keyboard_omni3/wheel_controller.py
+++ teleop_keyboard_omni3/wheel_controller.py
@@ -16,9 +16,14 @@ import tf
 import sys, select, termios, tty
 
 # # Usage: (left_wheel_velocity, back_wheel_velocity, right_wheel_velocity) = np.matmul(TWIST2WHEEL_VELOCITY, (x_velocity, y_velocity, turn_velocity))
-TWIST2WHEEL_VELOCITY = np.array([[-1/3, -1/np.sqrt(3), 1/3],
-                                 [2/3, 0, 1/3],
-                                 [-1/3, 1/np.sqrt(3), 1/3]])
+# Omnimodel 1
+# TWIST2WHEEL_VELOCITY = np.array([[-1/3, -1/np.sqrt(3), 1/3],
+#                                  [2/3, 0, 1/3],
+#                                  [-1/3, 1/np.sqrt(3), 1/3]])
+# Omnimodel 2
+TWIST2WHEEL_VELOCITY = np.array([[1/3, 1/np.sqrt(3), 1/3],
+                                 [-2/3, 0, 1/3],
+                                 [1/3, -1/np.sqrt(3), 1/3]])
 
 
 ROBOT_RADIUS = 0.7 # Based on +-0.04 rim shifts in main.urdf.xacro
@@ -34,7 +39,7 @@ def publish_commands(data):
     vth = data.angular.z * ROBOT_RADIUS # Back to linear speed from angular speed of robot
 
     # Since we rotated model 90 degrees clockwise we need to recompute TWIST2WHEEL_VELOCITY. But so far let's just substitute x and y
-    vx, vy = -vy, vx 
+    # vx, vy = -vy, vx # For omnimodel 1
     (left_wheel_velocity, back_wheel_velocity, right_wheel_velocity) = np.matmul(TWIST2WHEEL_VELOCITY, (vx, vy, vth))
 
     vell = Float64(left_wheel_velocity / ROBOT_WHEEL_RADIUS) # Velocity controller takes angular speed of wheel
